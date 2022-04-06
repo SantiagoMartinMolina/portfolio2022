d0122352c9cfdc129c5ee359fab205775750aacf
SantiagoMartinMolina <molina.santiago.martin@gmail.com>
2022-04-06 20:00:14 -0300

    Add a Navbar test to app

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,28 @@
+// import { useEffect, useRef } from "react";
 import "./App.css";
+// import Navbar from "./components/Navbar";
+import Navbar2 from "./components/Navbar2";
+import useScrollSettings from "./hooks/useScrollSettings";
+// import gsap from "gsap";
 
 function App() {
-  return <></>;
+  const scroller = useScrollSettings(true);
+
+  return (
+    <>
+      {/* <Navbar /> */}
+      <Navbar2 />
+      <div id="main-container" className="main-container" data-scroll-container>
+        <div style={{ height: "100vh" }}>hola</div>
+        <div style={{ height: "100vh" }}>hola</div>
+        <div id="prueba" style={{ height: "100vh" }}>
+          hola
+        </div>
+        <div style={{ height: "100vh" }}>hola</div>
+        <div style={{ height: "100vh" }}>hola</div>
+      </div>
+    </>
+  );
 }
 
 export default App;
