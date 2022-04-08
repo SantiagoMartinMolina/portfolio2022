9b7168c7297e956fc728ddf3e5f29898b01b9f0f
SantiagoMartinMolina <molina.santiago.martin@gmail.com>
2022-04-07 22:21:08 -0300

    Update App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,19 @@
-import { useEffect, useRef } from "react";
 import "./App.css";
-// import Navbar from "./components/Navbar";
-import Navbar2 from "./components/Navbar2";
+import Navbar from "./components/Navbar";
 import useScrollSettings from "./hooks/useScrollSettings";
-import gsap from "gsap";
+import Landing from "./components/Landing";
+import About from "./components/About";
 
 function App() {
-  const scroller = useScrollSettings(true);
+  useScrollSettings(true);
 
   return (
     <>
-      {/* <Navbar /> */}
-      <Navbar2 />
+      <Navbar />
       <div id="main-container" className="main-container" data-scroll-container>
+        <Landing />
+
+        <About />
         <div style={{ height: "100vh" }}>hola</div>
         <div style={{ height: "100vh" }}>hola</div>
         <div id="prueba" style={{ height: "100vh" }}>
