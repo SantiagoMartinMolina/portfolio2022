b7fc0aee3278c32997b3e9f0b0b818c7bbf3b0db
SantiagoMartinMolina <molina.santiago.martin@gmail.com>
2022-04-07 22:18:44 -0300

    Add a new navbar

---
+++ src/components/Navbar/index.tsx
@@ -0,0 +1,108 @@
+import { useEffect, useState } from "react";
+import { StyledNavbar2 } from "./styles";
+import gsap from "gsap";
+
+const Navbar = () => {
+  const [isActive, setIsActive] = useState(false);
+
+  useEffect(() => {
+    gsap.from(".menu-btn", {
+      y: 100,
+      duration: 0.5,
+    });
+
+    gsap.from(".logo span", {
+      y: 100,
+      duration: 0.5,
+    });
+  }, []);
+
+  const handleClick = () => {
+    if (isActive) {
+      handleMouseLeave();
+      return;
+    }
+    handleMouseEnter();
+  };
+  const handleMouseEnter = () => {
+    setIsActive(true);
+    gsap
+      .timeline()
+      .to(".nav-bg", {
+        scale: 1,
+        opacity: 1,
+        duration: 0.3,
+        ease: "Back.easeOut.config(1.7)",
+      })
+      .to(
+        ".menu a",
+        {
+          y: "0%",
+          opacity: 1,
+          duration: 0.02,
+          stagger: 0.05, // 0.1 seconds between when each "a" element starts animating
+        },
+        "<"
+      )
+      .to(".menu-btn", { backgroundColor: "#000", color: "#fafafa" }, "<");
+  };
+  const handleMouseLeave = () => {
+    setIsActive(false);
+
+    gsap.to(".nav-bg", {
+      scale: 0.9,
+      opacity: 0,
+      duration: 0.3,
+      ease: "Back.easeOut.config(1.7)",
+    });
+
+    gsap.to(".menu a", {
+      y: "100%",
+      opacity: 0,
+      duration: 0.1,
+      stagger: 0.05,
+    });
+
+    gsap.to(".menu-btn", {
+      backgroundColor: "#fafafa",
+      color: "#000",
+    });
+  };
+
+  return (
+    <StyledNavbar2>
+      <div>
+        <h2 className="logo">
+          <span>SM</span>
+        </h2>
+      </div>
+      <nav onMouseLeave={handleMouseLeave}>
+        <div className="menu-btn-container">
+          <button
+            onMouseEnter={handleMouseEnter}
+            onClick={handleClick}
+            className="menu-btn"
+          >
+            Menu
+          </button>
+        </div>
+        <ul className="menu">
+          <li>
+            <a href="#">Proyectos</a>
+          </li>
+          <li>
+            <a href="#">Habilidades</a>
+          </li>
+          <li>
+            <a href="#">Contacto</a>
+          </li>
+        </ul>
+        <div className="nav-bg">
+          <div></div>
+        </div>
+      </nav>
+    </StyledNavbar2>
+  );
+};
+
+export default Navbar;
---
+++ src/components/Navbar/styles.ts
@@ -0,0 +1,109 @@
+import styled from "styled-components";
+
+export const StyledNavbar2 = styled.header`
+  display: flex;
+  justify-content: space-between;
+  padding: 1rem;
+  position: fixed;
+  width: 100%;
+  top: 0;
+  left: 0;
+  z-index: 10;
+
+  .logo {
+    margin-top: 32px;
+    margin-left: 32px;
+    line-height: 0.75;
+    overflow: hidden;
+
+    span {
+      display: inline-block;
+      mix-blend-mode: difference;
+    }
+  }
+
+  nav {
+    padding: 2em;
+    border-radius: 15px;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    position: relative;
+
+    .menu-btn-container {
+      overflow: hidden;
+      z-index: 9;
+    }
+
+    .menu-btn {
+      background-color: inherit;
+      border: 1px solid #000;
+      border-radius: 10px;
+      padding: 0.25em 0.5em;
+      background-color: #fafafa;
+      text-align: center;
+      margin-bottom: 1em;
+      z-index: 10;
+      color: #000;
+      cursor: pointer;
+      user-select: none;
+      font-family: inherit;
+      text-transform: uppercase;
+    }
+
+    ul {
+      z-index: 10;
+      list-style-type: none;
+
+      li {
+        overflow: hidden;
+        transition: 0.3s;
+
+        &:hover {
+          transform: scale(1.1);
+        }
+      }
+
+      a {
+        text-decoration: none;
+        color: #000;
+        font-size: 1em;
+        display: flex;
+        overflow: hidden;
+        transition: 0.3s;
+        position: relative;
+        transform: translateY(100%);
+        opacity: 0;
+      }
+    }
+
+    .nav-bg {
+      position: absolute;
+      width: 100%;
+      height: 100%;
+      top: 0;
+      left: 0;
+      opacity: 0;
+      transform: scale(0.9);
+      z-index: 1;
+      div {
+        width: 100%;
+        height: 100%;
+        background: #fafafa;
+        border-radius: 20px;
+      }
+    }
+  }
+
+  @media (max-width: 1024px) {
+    padding: 1rem;
+
+    .logo {
+      margin-top: 16px;
+    }
+
+    nav {
+      padding: 1em;
+    }
+  }
+`;
