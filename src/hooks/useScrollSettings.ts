02be88a7827713226bfa7933482784f2045d1382
SantiagoMartinMolina <molina.santiago.martin@gmail.com>
2022-04-09 04:15:39 -0300

    Fix hook

--- src/hooks/useScrollSettings.ts
+++ src/hooks/useScrollSettings.ts
@@ -36,20 +36,21 @@ const useScrollSettings = (start: boolean) => {
 
     ScrollTrigger.scrollerProxy(scrollEl, {
       scrollTop(value) {
-        return arguments.length
-          ? locoScroll.scrollTo(value, 0, 0)
-          : locoScroll.scroll.instance.scroll.y;
-      }, // we don't have to define a scrollLeft because we're only scrolling vertically.
-      getBoundingClientRect() {
-        return {
-          top: 0,
-          left: 0,
-          width: window.innerWidth,
-          height: window.innerHeight,
-        };
+        if (locoScroll) {
+          return arguments.length
+            ? locoScroll.scrollTo(value, 0, 0)
+            : locoScroll.scroll.instance.scroll.y;
+        }
+        return null;
       },
-      // LocomotiveScroll handles things completely differently on mobile devices - it doesn't even transform the container at all! So to get the correct behavior and avoid jitters, we should pin things with position: fixed on mobile. We sense it by checking to see if there's a transform applied to the container (the LocomotiveScroll-controlled element).
-      pinType: scrollEl!.style.transform ? "transform" : "fixed",
+      scrollLeft(value) {
+        if (locoScroll) {
+          return arguments.length
+            ? locoScroll.scrollTo(value, 0, 0)
+            : locoScroll.scroll.instance.scroll.x;
+        }
+        return null;
+      },
     });
 
     const lsUpdate = () => {
