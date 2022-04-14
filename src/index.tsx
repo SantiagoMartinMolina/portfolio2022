c759f63184c81068845eb66592763ea081ef7670
SantiagoMartinMolina <molina.santiago.martin@gmail.com>
2022-04-14 20:43:10 -0300

    Change react version

--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,6 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import LanguageProvider from "./context/LanguageContext";
-// import { createRoot } from "react-dom/client";
 
 ReactDOM.render(
   <React.StrictMode>
@@ -13,13 +12,3 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById("root")
 );
-
-// const container = document.getElementById("root")!;
-// const root = createRoot(container);
-// root.render(
-//   <React.StrictMode>
-//     <LanguageProvider>
-//       <App />
-//     </LanguageProvider>
-//   </React.StrictMode>
-// );
